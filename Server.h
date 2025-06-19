3b00667a5eeaf307b63df3c50137498c6a6c0a20
André <151085951+DARR-1@users.noreply.github.com>
2025-06-18 18:16:47 -0600

    Actualizar Server.h

--- Server.h
+++ Server.h
@@ -1,9 +1,26 @@
 #pragma once
 
-#include <winsock2.h>
 #include <string>
 #include <cstring>
 #include <cstdlib>
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <Ws2tcpip.h>
+    #pragma comment(lib, "Ws2_32.lib")
+#else
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+    #include <netdb.h>
+    #include <errno.h>
+
+    #define INVALID_SOCKET (-1)
+    #define SOCKET_ERROR (-1)
+    typedef int SOCKET;
+#endif
 
 class Server
 {
@@ -19,11 +36,13 @@ public:
     int close();
 
 private:
+#ifdef _WIN32
     WSADATA wsaData;
+#endif
     SOCKET ListenSocket, AcceptSocket;
     u_short port = 27015;
     std::string hostname;
-    std::string ip; // ahora es std::string
+    std::string ip;
     sockaddr_in service;
     int error;
 };
